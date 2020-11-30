c5c936d38d896a970cdbaca2313f95dcd9d852a2
Artem Tsybulko <tsybulko.artem@gmail.com>
2020-12-01 00:05:05 +0300

    feat: menu active items

--- english-for-kids/src/js/moduls/Header/Header.js
+++ english-for-kids/src/js/moduls/Header/Header.js
@@ -1,34 +1,54 @@
 import create from '../../helpers/create'
+import { categories } from '../../data/cards'
 
 export default function Header(appendCards) {
   const menuButton = create('div', 'header__menu-button')
   const gameModeButton = create('div', 'header__mode-button')
   const menuList = create('ul', 'header__menu-burger__box-list')
-  const menuItems = [
-    'Main Page',
-    'Action (set A)',
-    'Action (set B)',
-    'Action (set C)',
-    'Adjective',
-    'Animal (set A)',
-    'Animal (set B)',
-    'Clothes',
-    'Emotion',
-  ]
+  const burgerMenu = create('div', 'header__menu-burger header__menu-burger-hide',
+    create('div', 'header__menu-burger__box', menuList))
+  const headerMenuContainer = create('div', 'header__menu', [menuButton, burgerMenu])
+  const menuItems = ['Main Page']
+  const menuElements = []
+
+  categories.forEach((i) => menuItems.push(i.categoryName))
+
+  function closeMenu() {
+    burgerMenu.classList.toggle('header__menu-burger-hide')
+    document.body.classList.toggle('scroll-lock')
+    if (!document.onclick) {
+      document.onclick = (e) => {
+        if (e.target.className === 'header__menu-burger') {
+          closeMenu()
+          document.onclick = null
+        }
+      }
+    } else {
+      document.onclick = null
+    }
+  }
+
+  function activeMenuItem(item) {
+    menuElements.forEach((i) => {
+      i.element.classList.remove('header__menu-burger-active')
+    })
+    const activeItem = menuElements.find((i) => i.menuItem === item)
+    activeItem.element.classList.add('header__menu-burger-active')
+  }
 
   menuItems.forEach((i) => {
     const el = create('li', 'header__menu-burger__box-list-item', i)
-    el.onclick = () => appendCards(i)
+    el.onclick = () => {
+      appendCards(i)
+      activeMenuItem(i)
+      closeMenu()
+    }
+    menuElements.push({ menuItem: i, element: el })
     menuList.append(el)
   })
 
-  const burgerMenu = create('div', 'header__menu-burger header__menu-burger-hide',
-    create('div', 'header__menu-burger__box', menuList))
-  const headerMenuContainer = create('div', 'header__menu', menuButton)
-
-  menuButton.onclick = () => {
-    headerMenuContainer.append(burgerMenu)
-  }
+  menuButton.onclick = closeMenu
+  activeMenuItem('Main Page')
 
   return create('div', 'header', [headerMenuContainer, gameModeButton])
 }
